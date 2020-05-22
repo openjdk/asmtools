351e056b7a4d55f5c87151017a72261963cf18cf
Leonid Kuskov <lkuskov@openjdk.org>
2020-05-22 19:06:35 +0000

    7902659: Correct unconditional call of System.exit in printVersion (Main.java)

--- src/org/openjdk/asmtools/Main.java
+++ src/org/openjdk/asmtools/Main.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -82,11 +82,10 @@ public class Main {
     }
 
     /**
-     * Prints the tools version and calls System.exit(0)
+     * Prints the tools version
      */
     public static void printVersion() {
-        System.err.println(ProductInfo.FULL_VERSION);
-        System.exit(0);
+        System.out.println(ProductInfo.FULL_VERSION);
     }
 
     /**
