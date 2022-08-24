f901a1c1b7e2a8b18f42c20932255ed0d60fe1e2
Jiri <jvanek@redhat.com>
2022-08-24 13:30:22 +0200

    Naive fix for enforced dot.suffix

--- src/org/openjdk/asmtools/common/ToolInput.java
+++ src/org/openjdk/asmtools/common/ToolInput.java
@@ -173,7 +173,7 @@ public interface ToolInput {
         @Override
         public String getFileName() {
             //get parent is used
-            return "stdin/in";
+            return "stdin/stdin";
         }
     }
 }
